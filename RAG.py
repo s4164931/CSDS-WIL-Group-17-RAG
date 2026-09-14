0be6f68ad41bd2aca9a22af387cdb9b07c059960
s4164931 <s4164931@student.rmit.edu.au>
2026-09-15 09:22:03 +1000

    Some websites for the RAG model

--- RAG.py
+++ RAG.py
@@ -1,2 +1,17 @@
 from deepagents import create_deep_agent
 from langchain.messages import HumanMessage
+
+electric_Energy_Docs = ["https://en.wikipedia.org/wiki/Electrical_energy",
+                        "https://www.thoughtco.com/electrical-energy-definition-and-examples-4119325",
+                        "https://www.eia.gov/energyexplained/electricity/the-science-of-electricity.php",
+                        "https://en.wikipedia.org/wiki/Electric_current",
+                        "https://www.britannica.com/technology/electric-circuit",
+                        "https://en.wikipedia.org/wiki/Voltage",
+                        "https://learn.sparkfun.com/tutorials/alternating-current-ac-vs-direct-current-dc/all",
+                        "https://www.wevolver.com/article/parallel-vs-series-circuits-differences-theory-and-practical-applications",
+                        "https://en.wikipedia.org/wiki/Watt",
+                        "https://cc-techgroup.com/blog/what-is-resistance/",
+                        "https://en.wikipedia.org/wiki/Electrolysis",
+                        "https://www.britannica.com/science/electricity"] 
+# electric energy, voltage, electric circuit, electric current, AC/DC, watts, parrallel vs series circuits, resistance, ohms, electrolysis,
+# electricity, energy
