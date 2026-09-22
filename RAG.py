9649e66f5ff11f8c57afe060b4badb583f2e6cb7
s4164931 <s4164931@student.rmit.edu.au>
2026-09-22 10:30:04 +1000

    Evaluation Questions

--- RAG.py
+++ RAG.py
@@ -15,9 +15,25 @@ from langchain.messages import HumanMessage
                         "https://www.britannica.com/science/electricity"]"""
 # electric energy, voltage, electric circuit, electric current, AC/DC, watts, parrallel vs series circuits, resistance, ohms, electrolysis,
 # electricity, energy
-# this was the old websites, which we mightnot use.
+# this was the old websites, which we might not use.
 
 # ___________________________________________
-# Evaluation questions in dict format (Luke), making 15. You guys can remove 5 or something if I made too many.
+# Evaluation questions in dict format (Luke), making 16. You guys can remove 6 or something if I made too many.
+# ___________________________________________
 
-eval_queations_dict = {}
+eval_questions_dict = {1:"What is one houshold item that is using a lot of energy consumption?",
+                       2:"How can I reduce my energy consumption?",
+                       3:"What is causing the most energy consumption? The fridge, the oven or the kettle?",
+                       4:"Would using solar panels reduce energy consumption?",
+                       5:"Would five solar panels be able to generate enough energy for all household items in the average household?",
+                       6:"What is one household item that is using little energy consumption?",
+                       7:"What are simple things that I can do right now to reduce energy consumption?",
+                       8:"Do lights use more energy consumption than fans?",
+                       9:"Would leaving everything on reduce energy consumption?", # this should give a no answer.
+                       10:"How many solar panels do we need for a large house with a lot of household items?", # a little ambiguous
+                       11:"What is the average energy consumption for any household?",
+                       12:"What would increase my energy consumption?",
+                       13:"Do solar panels use more energy consumption than traditional methods?",
+                       14:"What is using the least energy consumption? The lights, the TV or the fridge?",
+                       15:"How do I reduce my energy consumption, without having to decrease the quality of life?", # Maybe?
+                       16:"Would ten solar panels be able to generate enough energy for all household items in a large household?"}
